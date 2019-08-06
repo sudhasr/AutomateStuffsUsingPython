c790cbce95dd3ef06e8c5f1e37fdb6b8a651a94a
Sudha S R <sudha.ramc@gmail.com>
2019-08-06 13:32:08 -0700

    Open multiple links of search result

---
+++ lucky.py
@@ -0,0 +1,23 @@
+#! python3
+
+"""Opens several google search results"""
+
+import requests, sys, webbrowser, bs4
+
+print('Googling...') # display text while downloading the Google page
+res = requests.get('http://google.com/search?q=' + ' '.join(sys.argv[1:]))
+res.raise_for_status()
+
+# Retrieve top search result links.
+soup = bs4.BeautifulSoup(res.text, features = "html.parser")
+
+# Open a browser tab for each result.
+linkElems = soup.select('.r a')
+numOpen = min(5, len(linkElems))
+for i in range(numOpen):
+    webbrowser.open('http://google.com' + linkElems[i].get('href'))
+	
+	
+	
+#//*[@id="ember1894"]/button
+#	//*[@id="ember1894"]/button
