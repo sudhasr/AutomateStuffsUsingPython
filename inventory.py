3f35b4b102b6de6566f9e162182187d02b5a4a56
Sudha S R <sudha.ramc@gmail.com>
2019-08-06 13:32:36 -0700

    Inventory game

---
+++ inventory.py
@@ -0,0 +1,12 @@
+#! python3
+
+def displayInventory(inventory):
+	print("Inventory:")
+	item_total = 0
+	for k, v in inventory.items():
+		item_total = item_total + v
+		print(str(v)+' '+k)
+	print("Total number of items: " + str(item_total))
+
+stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+displayInventory(stuff)
